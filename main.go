6531d32631a49fb161ed43eb9ae969a4621cc1a7
Bosco Vallejo-Nágera Esteban <juan.vallejonagera@newryglobal.com>
2023-12-06 15:13:52 +0100

    part two done

--- main.go
+++ main.go
@@ -9,24 +9,17 @@ func beatsRecord(distance int, timePressed int, timeAvailable int) bool {
 }
 
 func main() {
-	// times := []int{7, 15, 30}
-	// distances := []int{9, 40, 200}
-	times := []int{50, 74, 86, 85}
-	distances := []int{242, 1017, 1691, 1252}
+	// time := 71530
+	// distance := 940200
+	time := 50748685
+	distance := 242101716911252
 
-	mul := 1
-	for i, v := range times {
-		sum := 0
-		for j := 1; j < v; j++ {
-			if beatsRecord(distances[i], j, v) {
-				sum += 1
-			}
-		}
-		if sum != 0 {
-			fmt.Println(sum)
-			mul *= sum
+	sum := 0
+	for j := 1; j < time; j++ {
+		if beatsRecord(distance, j, time) {
+			sum += 1
 		}
 	}
 
-	fmt.Println(mul)
+	fmt.Println(sum)
 }
